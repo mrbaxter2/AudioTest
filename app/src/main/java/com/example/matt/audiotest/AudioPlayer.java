028f5755f7fa5e56e7169f4b4f9ca05fd4f83940
Matt <mrbaxter2@bsu.edu>
2016-03-25 16:34:32 -0400

    1st

---
+++ app/src/main/java/com/example/matt/audiotest/AudioPlayer.java
@@ -0,0 +1,31 @@
+package com.example.matt.audiotest;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AudioPlayer extends AppCompatActivity {
+
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+
+        playButton = (Button) findViewById(R.id.buttonPlay);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AudioPlayer.this, AudioTest.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
