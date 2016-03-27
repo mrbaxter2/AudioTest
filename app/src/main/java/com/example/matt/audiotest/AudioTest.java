f0288143f1ee7537fa16074f48019ff111a3434c
Matt <mrbaxter2@bsu.edu>
2016-03-27 11:57:17 -0400

    2nd

---
+++ app/src/main/java/com/example/matt/audiotest/AudioTest.java
@@ -0,0 +1,29 @@
+package com.example.matt.audiotest;
+
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AudioTest extends AppCompatActivity {
+
+    MediaPlayer mySound;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mySound.release();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+        mySound = MediaPlayer.create(this, R.raw.topbottem);
+    }
+
+    public void playSound(View view) {
+        mySound.start();
+    }
+}
